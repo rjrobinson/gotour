d26bf5aa15dbfeb9d9bc536b762ad140c63cbabc
RJ Robinson <robertj.robinson@gmail.com>
2014-12-25 17:08:42 -0500

    added basic if statement

--- for.go
+++ for.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func sqrt(x float64) string {
+	if x < 0 {
+		return sqrt(-x) + "i"
+	}
+	return fmt.Sprint(math.Sqrt(x))
+}
 
 func main() {
-	for {
-		// infinite loop easily expressed
-	}
+	fmt.Println(sqrt(2), sqrt(-4))
 }
