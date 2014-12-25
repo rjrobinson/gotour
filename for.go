8626556976a57bd125d839f6289cddb81109d91b
RJ Robinson <robertj.robinson@gmail.com>
2014-12-25 17:04:09 -0500

    infinite loop easily expressed

--- for.go
+++ for.go
@@ -3,12 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	sum := 1
-	for sum < 1000 {
-		fmt.Println("Hello! The next number is....")
-		sum += sum
-		fmt.Println(sum)
-
+	for {
+		// infinite loop easily expressed
 	}
-	fmt.Println(sum)
 }
